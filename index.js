7b3917e16c38a0f68cac4b44b1522e86d8f2dbf5
mercysound <adekunleadewolemercy@gmail.com>
2023-09-05 21:27:40 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 // var firstPlayerName = prompt("first player what is your name")
 // var secondPlayerName = prompt("second player what is your name")
 // XturnCoun.innerText = firstPlayerName
-
+var firstPlayerName;
+var secondPlayerName;
+var playerOneScore = 0
+var playerTwoScore = 0
 var globalClick = "X";
 
+const joinGame = () =>{
+  firstPlayerName = prompt("Enter first player name")
+  secondPlayerName = prompt("Enter second player name")
+  document.getElementById("forplayer1").innerText = firstPlayerName
+  document.getElementById("forplayer2").innerText = secondPlayerName
+  changeTurn()
+
+  forJoin.style.display = "none"
+  forStart.style.display = "inline"
+}
+const startGame =  () =>{
+  // forJoin.style.display = "none"
+  forStart.style.display = "none"
+  forGameSur.style.display = "grid"
+  forReset.style.display = "inline"
+
+}
+const resetGame =  () =>{
+  location.reload()
+}
 const changeTurn = () =>{
   // if(globalClick == "X"){
   //   globalClick = "O"
   // }else if(globalClick == "O"){
   //   globalClick = "X"
   // }
+  // to check player name turn
+  globalClick == "X" ? document.getElementById("playTurn").innerHTML = firstPlayerName + " TURN (O)" : document.getElementById("playTurn").innerHTML  = secondPlayerName + " TURN (X)"
+  // document.getElementById("playTurn").innerHTML = globalClick + " turn"
   globalClick = globalClick == "X" ? "O" : "X"
-  document.getElementById("playTurn").innerHTML = globalClick + " turn"
   checkWinner()
 }
 
@@ -26,7 +51,8 @@ const checkWinner = () => {
   let fV8 = forClk8.innerText;
   let fV9 = forClk9.innerText;
   if((fV1 == "X" && fV2 == "X" && fV3 == "X") || (fV4 == "X" && fV5 == "X" && fV6 == "X") || (fV7 == "X" && fV8 == "X" && fV9 == "X") || (fV1 == "X" && fV4 == "X" && fV7 == "X") || (fV2 == "X" && fV5 == "X" && fV8 == "X") || (fV3 == "X" && fV6 == "X" && fV9 == "X") || (fV1 == "X" && fV5 == "X" && fV9 == "X") || (fV3 == "X" && fV5 == "X" && fV7 == "X")){
-    alert("X won")
+    alert(secondPlayerName + " Won")
+    document.getElementById("secPlayScr").innerText = playerTwoScore += 1
     forClk1.innerText = ""
     forClk2.innerText = ""
     forClk3.innerText = ""
@@ -36,9 +62,9 @@ const checkWinner = () => {
     forClk7.innerText = ""
     forClk8.innerText = ""
     forClk9.innerText = ""
-}
-   if((fV1 == "O" && fV2 == "O" && fV3 == "O") || (fV4 == "O" && fV5 == "O" && fV6 == "O") || (fV7 == "O" && fV8 == "O" && fV9 == "O") || (fV1 == "O" && fV4 == "O" && fV7 == "O") || (fV2 == "O" && fV5 == "O" && fV8 == "O") || (fV3 == "O" && fV6 == "O" && fV9 == "O") || (fV1 == "O" && fV5 == "O" && fV9 == "O") || (fV3 == "O" && fV5 == "O" && fV7 == "O")){
-    alert("O won")
+  }else if((fV1 == "O" && fV2 == "O" && fV3 == "O") || (fV4 == "O" && fV5 == "O" && fV6 == "O") || (fV7 == "O" && fV8 == "O" && fV9 == "O") || (fV1 == "O" && fV4 == "O" && fV7 == "O") || (fV2 == "O" && fV5 == "O" && fV8 == "O") || (fV3 == "O" && fV6 == "O" && fV9 == "O") || (fV1 == "O" && fV5 == "O" && fV9 == "O") || (fV3 == "O" && fV5 == "O" && fV7 == "O")){
+    alert(firstPlayerName+" WON")
+    document.getElementById("firPlayScr").innerText = playerOneScore += 1
     forClk1.innerText = ""
     forClk2.innerText = ""
     forClk3.innerText = ""
@@ -48,7 +74,19 @@ const checkWinner = () => {
     forClk7.innerText = ""
     forClk8.innerText = ""
     forClk9.innerText = ""
-  } 
+    
+  }else if((fV1 && fV2 && fV3 && fV4 && fV5 && fV6 && fV7 && fV8 && fV9) != ""){
+    alert("draw")
+    forClk1.innerText = ""
+    forClk2.innerText = ""
+    forClk3.innerText = ""
+    forClk4.innerText = ""
+    forClk5.innerText = ""
+    forClk6.innerText = ""
+    forClk7.innerText = ""
+    forClk8.innerText = ""
+    forClk9.innerText = ""
+  }
 }
 
 const clk1 = () =>{
